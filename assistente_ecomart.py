a93b8d84032afed9f82b92035df921090d6a7de7
Matheus Bazoti <mbazoti@outlook.com.br>
2024-03-04 14:11:17 -0300

    added personality to the bot and tools

--- assistente_ecomart.py
+++ assistente_ecomart.py
@@ -5,6 +5,7 @@ from time import sleep
 from helpers import *
 from selecionar_persona import *
 import json
+from tools_ecomart import *
 
 
 load_dotenv()
@@ -76,6 +77,7 @@ def criar_assistente(file_ids=[]):
                                 Além disso, acesse os arquivos associados a você e a thread para responder as perguntas.
                                 """,
                 model = modelo,
+                tools = minhas_tools,
                 file_ids = file_ids
         )
         return assistente
